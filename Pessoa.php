5ac6dc45ef8f3e8f3d3ddcd28ee440230ea8f02e
Jorge Clésio <jorgepebas@gmail.com>
2019-10-19 20:59:32 -0300

    Create Pessoa.php

---
+++ Pessoa.php
@@ -0,0 +1,11 @@
+Exemplo de uma Pessoa
+class Pessoa{ 
+  private $nome; 
+  private $cpf; 
+  private $data;
+private function editarNome{
+}
+public function editarPerfil{
+   editar_nome();
+}
+}
