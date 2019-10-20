ef6a7076d0a091acd8c1e3e96594776ef010b628
Jorge Clésio <jorgepebas@gmail.com>
2019-10-19 21:00:31 -0300

    Update Pessoa.php

--- Pessoa.php
+++ Pessoa.php
@@ -1,11 +1,15 @@
 Exemplo de uma Pessoa
+<?php
 class Pessoa{ 
-  private $nome; 
-  private $cpf; 
-  private $data;
-private function editarNome{
+    private $nome; 
+    private $cpf; 
+    private $data;
+  private function editarNome{
+
+  }
+  
+  public function editarPerfil{
+     editar_nome();
+  }
 }
-public function editarPerfil{
-   editar_nome();
-}
-}
+?>
